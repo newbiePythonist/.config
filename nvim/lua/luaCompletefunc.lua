7b266d43d8dc50de46385dabd1c16cf671377289
newbiePythonist <rametta.christian@gmail.com>
2024-01-20 23:17:11 +0100

    optimized custom completion func in lua

--- nvim/lua/luaCompletefunc.lua
+++ nvim/lua/luaCompletefunc.lua
@@ -7,13 +7,16 @@ function tableLen(t)
 end
 
 function tableContains(t, pattern)
-    for x in pairs(t) do
-        -- iterate over each subtable item to search for <pattern>
-        for j in pairs(t[x]) do
-            if t[x][j] == pattern then
-                return true
-            end
+    for _,x in ipairs(t) do
+        if x.word == pattern then 
+            return true
         end
+        -- iterate over each subtable item to search for <pattern>
+        -- for j in pairs(t[x]) do
+            -- if t[x][j] == pattern then
+                -- return true
+            -- end
+        -- end
     end
     return false
 end
@@ -69,32 +72,41 @@ function LuaCompleteWords(findstart, base)
         return start
     else
         local matches = {}
+        local added = {}
+        local length = 0
         -- iterate over all listed buffers
-        for bufnr=1,vim.fn.bufnr('$') do
+        for bufnr=0,vim.fn.bufnr('$') do
             -- if buffer is listed, iterate over all lines inside it
             if vim.fn.buflisted(bufnr) then
                 local bufname = vim.fn.bufname(bufnr)
                 -- don't show current bufname in the completion menu
-                if bufnr == vim.fn.bufnr() then
+                if bufnr == 0 then
                     bufname = ''
+                -- current buffer has already been iterated when bufnr was equal to 0, so let's skip it
+                elseif bufnr == vim.fn.bufnr() then
+                    bufname = ''
+                    goto continue
                 end
-                local lines = vim.api.nvim_buf_get_lines(bufnr, 0, vim.api.nvim_buf_line_count(bufnr), 0)
                 -- start iterating over lines in <bufnr>
-                for linenr in pairs(lines) do
-                    line = lines[linenr]
-                    -- iterate over each word in the line
-                    for word in string.gmatch(line, '%f[0-9A-Za-z_-]' .. base .. '[0-9A-Za-z_-]+%(?') do
-                        -- limit matches to a max of 30 entries
-                        if #matches >= 30 then
-                            return matches
-                        end
-                        -- check if matched word is already present, if not then add it
-                        if not tableContains(matches, word) then
-                            table.insert(matches, { word = word, kind = bufname })
-                        end
+                local lines = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, 0), ' ')
+                for match in string.gmatch(lines, '%f[0-9A-Za-z_-]' .. base .. '[0-9A-Za-z_-]+%(?') do
+                    -- limit matches to a max of 30 entries
+                    if #matches >= 30 then
+                        return matches
                     end
+                    -- check if matched word is already present, if not then add it (a key based table is used to avoid iterating over each item)
+                    if not added[match] then
+                        added[match] = true
+                        length = length + 1
+                        table.insert(matches, { word = match, kind = bufname })
+                    end
+                    ----- <OLD CODE> -----
+                    -- if not tableContains(matches, match) then
+                    -- table.insert(matches, { word = match, kind = bufname })
+                    -- end
                 end
             end
+            ::continue::
         end
         return matches
     end
